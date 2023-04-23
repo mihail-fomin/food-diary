5d013984a7395647be90e1bf3c246d1a95caa304
misha133kv <mihail_fomin_95@mail.ru>
2023-04-23 16:03:49 +0500

    added btn and styles

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -6,20 +6,20 @@ export default function AddNote() {
 	return (
 		<>
 			<form className="flex gap-2 mt-4">
-				<select>
+				<select className="border-gray-200 rounded-lg">
 					<option>Breakfast</option>
 					<option>Lunch</option>
 					<option>Dinner</option>
 				</select>
-				<input type='date' />
-				<fieldset className='inline-flex'>
+				<input type='date' className="border-gray-200 rounded-lg" />
+				<fieldset className='inline-flex '>
 					<input
-						className="rounded-l"
-						type='number' />
-					<div
-						className="p-2 bg-gray-200 rounded-r"
-					>kcal</div>
+						className="w-20 border-gray-200 rounded-l-lg"
+						type='number'
+						step='100' />
+					<div className="p-2 bg-gray-200 rounded-r">kcal</div>
 				</fieldset>
+				<button className="p-2 ml-2 text-white rounded-lg bg-sky-600">Add</button>
 			</form>
 		</>
 	)
